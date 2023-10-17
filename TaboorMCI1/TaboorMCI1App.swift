d4398f459c41ff68d9e575d089ca35fab2929e83
MzoonAbdullaziz <mzoonabdullaziz8@gmail.com>
2023-10-17 22:55:11 +0300

    Initial Commit

---
+++ TaboorMCI1/TaboorMCI1App.swift
@@ -0,0 +1,17 @@
+//
+//  TaboorMCI1App.swift
+//  TaboorMCI1
+//
+//  Created by Mzoon AlRubaish on 02/04/1445 AH.
+//
+
+import SwiftUI
+
+@main
+struct TaboorMCI1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ TaboorMCI1UITests/TaboorMCI1UITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  TaboorMCI1UITestsLaunchTests.swift
+//  TaboorMCI1UITests
+//
+//  Created by Mzoon AlRubaish on 02/04/1445 AH.
+//
+
+import XCTest
+
+final class TaboorMCI1UITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
